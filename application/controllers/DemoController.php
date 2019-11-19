ac8e38d6569b0f50db1d92a0dfaac1ffd26442c6
jorRabbit <dhe_suhendra@live.com>
2019-11-19 09:45:03 +0700

    update demo controller dan viewnya

--- application/controllers/DemoController.php
+++ application/controllers/DemoController.php
@@ -6,6 +6,36 @@ class DemoController extends CI_Controller
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('demo/index');
+	}
+
+	public function customdash()
+	{
+		$this->load->view('demo/custom-dashboard');
+	}
+
+	public function element()
+	{
+		$this->load->view('demo/element');
+	}
+
+	public function form()
+	{
+		$this->load->view('demo/form');
+	}
+
+	public function login()
+	{
+		$this->load->view('demo/login');
+	}
+
+	public function register()
+	{
+		$this->load->view('demo/register');
+	}
+
+	public function table()
+	{
+		$this->load->view('demo/table');
 	}
 }
