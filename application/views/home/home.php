edfd01467537d7173a386cddecea71f642e713cd
jorRabbit <dhe_suhendra@live.com>
2019-11-19 10:33:59 +0700

    membuat routes home dan content dan view home

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,1 +1,154 @@
-ini home
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12">
+            <span class="page-title red">
+                <h2>Dashboard</h2>
+            </span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <span class="page-title yellow">
+                <h4>Dashboard Summary V.1</h4>
+            </span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <a href="#">
+                <div class="banner-block red">
+                    <div class="block-content">
+                        <span class="banner-icon fa-stack fa-3x">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-inbox fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <div class="banner-content">
+                            <div class="title">50</div>
+                            <div class="sub-title">New Mails</div>
+                        </div>
+                        <div class="clear-both"></div>
+                    </div>
+
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <a href="#">
+                <div class="banner-block yellow">
+                    <div class="block-content">
+                        <span class="fa-stack fa-3x">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-comments fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <div class="banner-content">
+                            <div class="title">23</div>
+                            <div class="sub-title">New Message</div>
+                        </div>
+                        <div class="clear-both"></div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <a href="#">
+                <div class="banner-block green">
+                    <div class="block-content">
+                        <span class="fa-stack fa-3x">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-tags fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <div class="banner-content">
+                            <div class="title">280</div>
+                            <div class="sub-title">Product View</div>
+                        </div>
+                        <div class="clear-both"></div>
+                    </div>
+
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <a href="#">
+                <div class="banner-block blue">
+                    <div class="block-content">
+                        <span class="fa-stack fa-3x">
+                            <i class="fa fa-circle fa-stack-2x"></i>
+                            <i class="fa fa-share-alt fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <div class="banner-content">
+                            <div class="title">16</div>
+                            <div class="sub-title">Share</div>
+                        </div>
+                        <div class="clear-both"></div>
+                    </div>
+
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <span class="page-title">
+                <h4>Dashboard Summary V.2</h4>
+            </span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <a href="#">
+                <div class="banner-block-v2 red">
+                    <div class="block-content">
+                        <div class="banner-content">
+                            <div class="title">50</div>
+                            <div class="sub-title">New Mails</div>
+                        </div>
+                        <div class="clear-both"></div>
+                    </div>
+
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <a href="#">
+                <div class="banner-block-v2 yellow">
+                    <div class="block-content">
+                        <div class="banner-content">
+                            <div class="title">23</div>
+                            <div class="sub-title">New Message</div>
+                        </div>
+                        <div class="clear-both"></div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <a href="#">
+                <div class="banner-block-v2 green">
+                    <div class="block-content">
+                        <div class="banner-content">
+                            <div class="title">280</div>
+                            <div class="sub-title">Product View</div>
+                        </div>
+                        <div class="clear-both"></div>
+                    </div>
+
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <a href="#">
+                <div class="banner-block-v2 blue">
+                    <div class="block-content">
+                        <div class="banner-content">
+                            <div class="title">16</div>
+                            <div class="sub-title">Share</div>
+                        </div>
+                        <div class="clear-both"></div>
+                    </div>
+
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
